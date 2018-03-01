459a41308d1478829d2ea3c770fe24a9659599b7
aaa <it-z@tut.by>
2018-03-01 18:20:38 +0300

    here is your change

--- src/index.js
+++ src/index.js
@@ -1,5 +1,39 @@
-// PLEASE DON'T change function name
+
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+   let onHands = currency;
+   
+   let coins = {};
+   
+	if (onHands>0 && onHands<10001) {
+     
+     if (onHands>=50) { 
+       coins["H"] = ~~(onHands/50);
+       onHands %= 50;    
+     }
+     
+     if (onHands>=25) { 
+       coins["Q"] = ~~(onHands/25);
+       onHands %= 25;    
+     }
+     
+     if (onHands>=10) { 
+	 coins["D"] = ~~(onHands/10);
+       onHands %= 10;    
+     }
+     
+     if (onHands>=5) { 
+       coins["N"] = ~~(onHands/5);
+       onHands %= 5;    
+     }
+     if (onHands>=1) { 
+       coins["P"] = onHands;   
+     }       
+     
+	}
+   
+
+	if (onHands>10000) {
+           coins = {error: "You are rich, my friend! We don't have so much coins for exchange"};
+            }
+return coins;
 }
